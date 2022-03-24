b4882c53ba5f1fbeda419c15ca35c98e6fb1456b
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-24 12:30:15 +0100

    add js filter by tags

--- exercise.js
+++ exercise.js
@@ -2,7 +2,20 @@
 const cardsContainer = document.querySelector("[data-js=cards]");
 const form = document.querySelector("[data-js=form]");
 
-let cards = [];
+let cards = [
+  {
+    question: "What is HTML?",
+    tags: ["html", "basic", "web"],
+  },
+  {
+    question: "What is CSS?",
+    tags: ["css", "basic", "web"],
+  },
+  {
+    question: "What is Git?",
+    tags: ["git", "shell"],
+  },
+];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -27,45 +40,18 @@ form.addEventListener("submit", (event) => {
   questionElement.focus();
 });
 
-function renderCards() {
-  cardsContainer.innerHTML = "";
 
-  cards.forEach((card) => {
-    const cardElement = document.createElement("li");
-    cardElement.className = "card";
-    cardElement.innerHTML = `
-      <p>${card.question}</p>
-      <p>${card.answer}</p>
-      <ul role="list" class="card__tag-list">
-        ${card.tags.map((tag) => `<li class="card__tag">${tag}</li>`).join("")}
-      </ul>
-    `;
-    cardsContainer.append(cardElement);
-  });
-}
+console.log(cards);
 //------fromForm.js-finish--------------------------------------------
 //------filterByTag.js------------------------------------------------
-console.clear();
+//console.clear();
 
 const filterForm = document.querySelector("[data-js=filter-form]");
-const cardsContainer = document.querySelector("[data-js=cards]");
+
 
 let currentFilter = "all";
 
-const cards = [
-  {
-    question: "What is HTML?",
-    tags: ["html", "basic", "web"],
-  },
-  {
-    question: "What is CSS?",
-    tags: ["css", "basic", "web"],
-  },
-  {
-    question: "What is Git?",
-    tags: ["git", "shell"],
-  },
-];
+
 
 filterForm.addEventListener("change", () => {
   currentFilter = filterForm.elements["tag-filter"].value;
@@ -76,12 +62,9 @@ renderCards();
 
 function renderCards() {
   cardsContainer.innerHTML = "";
-
-  cards
-    .filter(
-      (card) => card.tags.includes(currentFilter) || currentFilter === "all"
-    )
-    .forEach((card) => {
+  //------------filter-------------------
+  cards.filter((card) => card.tags.includes(currentFilter) || currentFilter === "all"
+    ).forEach((card) => {
       const cardElement = document.createElement("li");
       cardElement.className = "card";
       cardElement.innerHTML = `
